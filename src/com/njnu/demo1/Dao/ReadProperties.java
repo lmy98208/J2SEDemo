fa69830618a1c0b530e649acf4427800abcb51d8
lmy98208 <924686743@qq.com>
2019-07-02 09:28:10 +0800

    20190702Day 1

---
+++ src/com/njnu/demo1/Dao/ReadProperties.java
@@ -0,0 +1,32 @@
+package com.njnu.demo1.Dao;
+
+import java.util.Properties;
+
+/**
+ * 读文件耗费资源——单例模式
+ */
+public class ReadProperties {
+    //静态对象，默认null
+    private static ReadProperties readProperties;
+    public String dburl="";
+    public String username="";
+    public String pwd="";
+    private ReadProperties(){readFile();}
+    public static ReadProperties getInstance(){
+        if(readProperties==null){
+            readProperties=new ReadProperties();
+        }
+        return readProperties;
+    }
+    private void readFile(){
+        Properties properties=new Properties();
+        try {
+            properties.load(getClass().getResourceAsStream("/com/njnu/demo1/db.properties"));
+            dburl=properties.getProperty("jdbc.url");
+            username=properties.getProperty("jdbc.username");
+            pwd=properties.getProperty("jdbc.password");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
